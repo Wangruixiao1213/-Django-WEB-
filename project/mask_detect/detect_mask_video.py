f58fe7f2485197bddc28112781000f3fb11470cb
王睿潇 <847573508@qq.com>
2021-11-22 19:05:01 +0800

    Update detect_mask_video.py

--- project/mask_detect/detect_mask_video.py
+++ project/mask_detect/detect_mask_video.py
@@ -29,16 +29,14 @@ maskNet = load_model("static/model/mask_detector.model")
 # def login():
 
 # frame为图片（视频流中的一帧） faceNet与maskNet分别为人脸识别模型和口罩识别模型
-def detect_and_predict_mask(frame, faceNet, maskNet):
+def MaskLocsAndPres(frame, faceNet, maskNet):
 
-    # 窗口的长和宽
+    # 窗口每一帧的长和宽
     (h, w) = frame.shape[:2]
-
     #windowPara = (h,w)
 
     #对图像进行预处理 frame是捕获到的图片 1.0是缩放比例（选择不缩放） 裁剪大小为（244，244）这么大 最后一个元组降低光照的影响    !!!opencv
     blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224), (104.0, 177.0, 123.0))
-
     # print(blob)
     # print('****')
     faceNet.setInput(blob)
@@ -49,8 +47,8 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
     # 初始化三个列表 分别存放处理后的face ; face坐标 ;对于face的mask检验预测值
     faces = []
-    locs = []
-    preds = []
+    locations = []
+    predicts = []
 
     # 以下为人脸识别的代码块，来自于网络
     for i in range(0, detections.shape[2]):
@@ -62,14 +60,14 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
             # box类似于容器 找出face框的坐标
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+            (LTX, LTY, RBX, RBY) = box.astype("int")
 
-            # 进一步确认 出现的face框同样出现在镜头中
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+            # 进一步确认 出现的face框同样出现在镜头中 left-top right-bottom
+            (LTX, LTY) = (max(0, LTX), max(0, LTY))
+            (RBX, RBY) = (min(w - 1, RBX), min(h - 1, RBY))
 
             # 裁剪出来face图
-            face = frame[startY:endY, startX:endX]
+            face = frame[LTY:RBY, LTX:RBX]
             # 脸变成黑白图
             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             # 缩放脸大小 224*224 网路的输入
@@ -79,67 +77,64 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
             # 数据处理完放入数组中 在口罩识别备用
             faces.append(face)
-            locs.append((startX, startY, endX, endY))
+            locations.append((LTX, LTY, RBX, RBY))
 
     # faces中存有值 即识别出人脸
     if len(faces) > 0:
         # 进行口罩检测
         faces = np.array(faces, dtype="float32")
-        preds = maskNet.predict(faces, batch_size=32)
+        predicts = maskNet.predict(faces, batch_size=32)
 
-    # 返回元组 (坐标列表,预测值列表) 坐标列表中每个元素为人脸识别框对角线顶点坐标（共四个坐标） 预测值列表中每个元素为二元组（戴口罩概率,不戴口罩概率）
-    return (locs, preds)
+    # 返回元组 (坐标列表,预测值列表) 坐标列表中每个元素为人脸识别框对角线顶点坐标（共四个坐标） 预测值列表中每个元素为二元组(戴口罩概率,不戴口罩概率)
+    return (locations, predicts)
 
 
 if __name__ == '__main__':
 
     #
-    print("[INFO] starting video stream...")
+    print("Opening the camera.....")
     # VideoCapture参数为0代表笔记本摄像头 为1代表其他摄像头 其他数字类推 代码获取视频流
-    webcam = cv2.VideoCapture(0)
-    if not webcam.isOpened():
+    webCamera = cv2.VideoCapture(0)
+    if not webCamera.isOpened():
         raise Exception("There is something wrong with the camera!")
 
     # 设置窗口分辨率 1920 x 1080
-    webcam.set(3,1920)
-    webcam.set(4,1080)
+    webCamera.set(3,1920)
+    webCamera.set(4,1080)
 
 
     # 循环读取视频流每一帧
     while True:
         # 获取摄像头 ret为true or false frame为视频流当前帧
-        ret, frame = webcam.read()
+        ret, frame = webCamera.read()
 
         # 检测是否佩戴口罩 先检测人脸，再识别口罩 三个帧率分别为 摄像头传来的实时帧 人脸识别模型 口罩识别模型 （其中人脸识别模型来自于网络 口罩识别模型为自己训练而得）
-        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+        (locations, predicts) = MaskLocsAndPres(frame, faceNet, maskNet)
 
 
         # 找出每一个人脸的位置并裁剪出框以及显示口罩识别的结果
-        for (box, pred) in zip(locs, preds):
+        for (border, predict) in zip(locations, predicts):
 
             # 对应人脸识别后裁剪出的人脸坐标框的对角线顶点
-            (startX, startY, endX, endY) = box
+            (LTX, LTY, RBX, RBY) = border
 
-            #这里mask和withoutMask是两个预测的概率 加和为1
+            #这里mask和noMask是两个预测的概率 加和为1
+            (mask, noMask) = predict
 
-            (mask, withoutMask) = pred
-
-
-
-            if mask > withoutMask:
+            if mask > noMask:
                 label = 'with mask'  # 想做成中文标签 但是cv2对于中文并不支持
-                color = (0, 255, 0)  # green
+                color = (0, 255, 0)  # green 象征着美好
             else:
                 label = "without mask"
-                color = (0, 0, 255)  # red
+                color = (0, 0, 255)  # red 象征着不美好
                 # 利用线程处理 否则因为执行Beep时间过长 视觉效果上会丢帧
                 t = Thread(target=task)
                 t.start()
                 # winsound.Beep(600,300)
 
             # 展示标签
-            cv2.putText(frame, label, (startX+100, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+            cv2.putText(frame, label, (LTX+100, LTY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (LTX, LTY), (RBX, RBY), color, 2)
 
         # imgName = "static/photo/frame.jpg"
         # cv2.imwrite(imgName,frame)
@@ -150,5 +145,5 @@ if __name__ == '__main__':
         if key == ord("q") or key == ord("Q"):
             break
     #关闭视频流
-    webcam.release()
+    webCamera.release()
     cv2.destroyAllWindows()
