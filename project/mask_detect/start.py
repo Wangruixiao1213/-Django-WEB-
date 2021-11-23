1a1a018c11eb1fcfd34e60149573cc030f12c8c7
王睿潇 <847573508@qq.com>
2021-11-23 23:08:00 +0800

    info

--- project/mask_detect/start.py
+++ project/mask_detect/start.py
@@ -2,4 +2,5 @@ import os
 # 通过start来启动
 if __name__ == '__main__':
     runCmd = 'python manage.py runserver 0.0.0.0:8000'
+    # Quit the server with CTRL-BREAK  so where is break?
     os.system(runCmd)
